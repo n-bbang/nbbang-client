4bcd4be581bd98b37b15b9dec0a74d966d05dd12
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-09-12 22:13:47 +0900

    fix: Home 컴포넌트에서 Header 참조 삭제

--- src/components/screen/Home/index.tsx
+++ src/components/screen/Home/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import Header from '../../shared/components/Header';
 
 interface HomeProps {
   navigation:any;
