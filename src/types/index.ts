4c8a339f6ac90c026f9e63ccd502f7f78f90eaa2
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-09-22 19:16:06 +0900

    feat: UserInterface 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,25 @@
-export interface CategoryInterface{
-    categoryId: number;
-    categoryName:string;
+export interface CategoryInterface {
+  categoryId: number;
+  categoryName: string;
 }
-export interface RoomInterface{
-    roomId:number;
-    platformId:number;
-    bossUserId:number;
-    roomName?:string;
-    totalPrice?:number;
-    personalPrice?:number
-    maxUser:number;
-    recentPayment?:Date;
+export interface RoomInterface {
+  roomId: number;
+  platformId: number;
+  bossUserId: number;
+  roomName?: string;
+  totalPrice?: number;
+  personalPrice?: number;
+  maxUser: number;
+  recentPayment?: Date;
+}
+
+export interface UserInterface {
+  userId: number;
+  loginId: string;
+  passwd: string;
+  name: string;
+  nickname?: string;
+  gender: number;
+  phoneNumber: string;
+  account?: string;
 }
