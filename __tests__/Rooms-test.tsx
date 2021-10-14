5f5877a925f90d28eb0ded566fa1fd71901e8b88
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-10-14 23:09:33 +0900

    test: Rooms 컴포넌트 snapshot test

---
+++ __tests__/Rooms-test.tsx
@@ -0,0 +1,15 @@
+import 'react-native';
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import Rooms from '../src/components/screen/Home/Rooms';
+import {getDummyRooms} from '../src/shared/functions/makeDummyData';
+
+describe('Rooms', () => {
+  it('rendering test', () => {
+    const rendered = render(
+      <Rooms rooms={getDummyRooms(5)} />,
+    );
+
+    expect(rendered).toMatchSnapshot();
+  });
+});
